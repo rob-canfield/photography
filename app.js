7b2138fc1b1f15562957b259c7a3c79e4894b069
Rob Canfield <git@livesenjoyable.com>
2020-04-21 11:51:57 -0600

    connected mongoDB cluster

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
 const express = require('express');
 const app = express();
+
+const mongoose = require('mongoose');
+
 require('dotenv').config();
+
+mongoose.connect(
+    process.env.MONGO_URI,
+    {useNewUrlParser: true}
+)
+.then(() => console.log('DB Connected'));
+
+mongoose.connect.on('error', error => {
+    console.log(`DB connection error: ${error.message}`)
+});
 
 app.get('/', (request, response) => {
     response.send('hello from node');
