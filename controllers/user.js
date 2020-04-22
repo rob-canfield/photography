16c81dbfcf72e633664c1e1aacf9fec482ebd29c
Rob Canfield <git@livesenjoyable.com>
2020-04-22 12:29:11 -0600

    updated error message for duplicate user singup. hid hashed password and salt from response JSON.

--- controllers/user.js
+++ controllers/user.js
@@ -1,14 +1,17 @@
 const User = require('../models/user')
+const {errorHandler} = require('../helpers/dbErrorHandler')
 
 exports.signup = (request, response) => {
-    console.log("request.body", request.body);
+    // console.log("request.body", request.body);
     const user = new User(request.body);
     user.save((error, user) => {
         if (error) {
             return response.status(400).json({
-                error
+                error: errorHandler(error) 
             });
         }
+        user.salt = undefined
+        user.hashed_password = undefined
         response.json({
             user
         });
