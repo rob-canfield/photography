a9dcd31a4ac5b84e675c7140a4a31c004df983de
Rob Canfield <git@livesenjoyable.com>
2020-04-21 20:48:00 -0600

    added user model

--- models/user.js
+++ models/user.js
@@ -1,36 +1,64 @@
 const mongoose = require('mongoose');
 const crypto = require('crypto');
-const uuid = require('uuid');
+const uuidv1 = require('uuid');
 
 const userSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        trim: true,
-        required: true,
-        maxlength: 32
-    },
-    email: {
-        type: String,
-        trim: true,
-        required: true,
-        unique: 32
-    },
-    hashed_password: {
-        type: String,
-        trim: true
-    },
-    about: {
-        type: String,
-        trim: true
-    },
-    salt: String,
-    role: {
-        type: Number,
-        default: 0
-    },
-    history: {
-        tyoe: Array,
-        default: []
-    }
-}, {timestamps: true}
+        name: {
+            type: String,
+            trim: true,
+            required: true,
+            maxlength: 32
+        },
+        email: {
+            type: String,
+            trim: true,
+            required: true,
+            unique: 32
+        },
+        hashed_password: {
+            type: String,
+            trim: true
+        },
+        about: {
+            type: String,
+            trim: true
+        },
+        salt: String,
+        role: {
+            type: Number,
+            default: 0
+        },
+        history: {
+            tyoe: Array,
+            default: []
+        }
+    }, 
+    { timestamps: true }
 );
+
+// virtual field
+userSchema.virtual('password')
+.set(function(password) {
+    this._password = password
+    this.salt = uuidv1()
+    this.hashed_password = this.encryptPassword(password)
+})
+.get(function(){
+    return this.password
+})
+
+userSchema.methods = {
+    encryptPassword: function(password) {
+        if(!password) return '';
+        try {
+            return crypto
+            .createHmac('sha1', this.salt)
+            .update(password)
+            .digeest("hex");
+        } catch(error) {
+            return "";
+        }
+    }
+}
+
+module.exports = mongoose.model("User", userSchema);
